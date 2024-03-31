afed791f4a9b7fa895720dfdaba91c86205aba0d
chaker ramdani <limio.viviok@gmail.com>
2024-03-31 16:19:58 +0100

    fixing a bug

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -31,7 +31,7 @@ const ProductProvider = ({ children }) => {
     const getData = async () => {
       dispatch({ type: 'FETCH_INIT' })
       try {
-        const res = await fetch('/public/data/products.json')
+        const res = await fetch('/dist/data/products.json')
         if (!res.ok) {
           throw new Error('Failed to fetch data')
         }
